b1e6e0aefc1c6b028d585e390d89e79d607574ba
sourcyed <31018609+sourcyed@users.noreply.github.com>
2024-08-17 20:50:10 +0300

    Added the frontend to the repo

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
 
 const requestLogger = (request, response, next) => {
     console.log('Method: ', request.method)
