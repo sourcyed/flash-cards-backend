efb5e83c200af7833d4a9ad3400acc026ddc6a3e
sourcyed <31018609+sourcyed@users.noreply.github.com>
2024-08-29 17:06:22 +0300

    Resets the picture property when a word is updated

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ app.post('/api/words', (request, response) => {
 
 app.put('/api/words/:id', (request, response) => {
     const body = request.body
-    const word = { word: body.word, meaning: body.meaning, sentence: body.sentence }
+    const word = { word: body.word, meaning: body.meaning, sentence: body.sentence, picture: body.picture }
     Word.findByIdAndUpdate(request.params.id, word, { new: true }).then(updatedWord => {
         response.json(updatedWord)
     })
