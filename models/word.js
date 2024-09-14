61ef040690ac22ee14779c61ff51ebfab8ee12ce
sourcyed <31018609+sourcyed@users.noreply.github.com>
2024-09-14 17:29:58 +0300

    Changed sentence restrictions

--- models/word.js
+++ models/word.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const mongoose = require('mongoose')
 
 mongoose.set('strictQuery', false)
@@ -25,7 +26,6 @@ const wordSchema = new mongoose.Schema({
     },
     sentence: {
         type: String,
-        minLength: 2,
         required: false
     },
     picture: {
