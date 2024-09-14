638d1c940b9f54ac5f2e4a1744109ddeaeb30b7a
sourcyed <31018609+sourcyed@users.noreply.github.com>
2024-09-14 17:38:21 +0300

    Added morgan for logging

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express = require('express')
+const morgan = require('morgan')
 const cors = require('cors')
 const Pexels = require('pexels')
 const mongoose = require('mongoose')
@@ -27,20 +28,11 @@ catch (err) {
     console.log(err) 
 }
 
-app.use(cors())
 app.use(express.json())
 app.use(express.static('dist'))
-
-const requestLogger = (request, response, next) => {
-    console.log('Method: ', request.method)
-    console.log('Path: ', request.path)
-    console.log('Body: ', request.body)
-    console.log('---')
-    next()
-}
-
-app.use(requestLogger)
-
+app.use(cors())
+morgan.token('body', function (req, res) { JSON.stringify(req.body); return JSON.stringify(req.body) })
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.get('/api/words', (request, response) => {
     Word.find({}).then(ws => response.json(ws))
