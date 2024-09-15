5f9912d97b64def8de937ac6171fdf9b89d22914
sourcyed <31018609+sourcyed@users.noreply.github.com>
2024-09-15 13:14:26 +0300

    Updated eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,6 +19,15 @@ export default [
       '@stylistic/js': stylisticJs
     },
     rules: {
+      'eqeqeq': 'error',
+      'no-trailing-spaces': 'error',
+      'object-curly-spacing': [
+        'error', 'always'
+      ],
+      'arrow-spacing': [
+        'error', { 'before': true, 'after': true },
+      ],
+      'no-console': 'off',
       '@stylistic/js/indent': [
         'error',
         2
@@ -35,6 +44,9 @@ export default [
         'error',
         'never'
       ],
-    }
+    },
   },
+  {
+    ignores: ["dist/**"],
+  }
 ];
