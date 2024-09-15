ac9fe108322a5e8f34a2b8a9852162caf7f5a782
sourcyed <31018609+sourcyed@users.noreply.github.com>
2024-09-15 13:15:38 +0300

    Fixed line break style

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -34,7 +34,7 @@ export default [
       ],
       '@stylistic/js/linebreak-style': [
         'error',
-        'unix'
+        'windows'
       ],
       '@stylistic/js/quotes': [
         'error',
