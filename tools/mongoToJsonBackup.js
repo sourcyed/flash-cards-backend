22bfe2c570039ef1b78c17dc11c4093a89adffd9
sourcyed <31018609+sourcyed@users.noreply.github.com>
2024-10-05 16:39:30 +0300

    Moved tool scripts to separate folder

---
+++ tools/mongoToJsonBackup.js
@@ -0,0 +1,17 @@
+const fs = require('fs')
+const Word = require('../models/word')
+
+const backup = 'backup.json'
+
+Word.find({}).then(words => {
+  words.map(w => {
+    return {
+      word: w.word,
+      meaning: w.meaning,
+      picture: w.picture,
+      id: w.id
+    }
+  })
+  fs.writeFileSync(backup, JSON.stringify( { words } ))
+  console.log(words.length, 'words saved')
+})
