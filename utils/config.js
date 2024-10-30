d4135b3b87c41596d66ea01e6c986b37577d99d1
sourcyed <31018609+sourcyed@users.noreply.github.com>
2024-10-30 20:12:20 +0200

    Add test database to config

--- utils/config.js
+++ utils/config.js
@@ -1,10 +1,13 @@
 require('dotenv').config()
 
 const PORT = process.env.PORT
-const MONGODB_URI = process.env.MONGODB_URI
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY
 const PEXELS_API_KEY = process.env.PEXELS_API_KEY
 const PASSWORD = process.env.PASSWORD
+
+const MONGODB_URI = process.env.NODE_ENV === 'test'
+  ? process.env.TEST_MONGODB_URI
+  : process.env.MONGODB_URI
 
 module.exports = {
   PORT,
