0392032025897c4bde552a441c14cf5917eee8de
sourcyed <31018609+sourcyed@users.noreply.github.com>
2025-02-22 13:11:00 +0000

    Add support for SSL and HTTPS

--- index.js
+++ index.js
@@ -2,8 +2,24 @@
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 const app = require('./app')
+const https = require('https')
+const fs = require('fs')
 
-const PORT = config.PORT || 3001
-app.listen(PORT, () => {
+const DOMAIN = config.DOMAIN 
+let PORT = config.PORT || 3001
+
+let server = app
+
+if (DOMAIN) {
+  const options = {
+      key: fs.readFileSync(`/etc/letsencrypt/live/${DOMAIN}/privkey.pem`),
+      cert: fs.readFileSync(`/etc/letsencrypt/live/${DOMAIN}/cert.pem`),
+      ca: fs.readFileSync(`/etc/letsencrypt/live/${DOMAIN}/fullchain.pem`)
+  }
+  server = https.createServer(options,app)
+  PORT = 443
+}
+
+server.listen(PORT, () => {
   logger.info(`Server running on port ${PORT}`)
 })
--- utils/config.js
+++ utils/config.js
@@ -4,6 +4,7 @@ const PORT = process.env.PORT
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY
 const PEXELS_API_KEY = process.env.PEXELS_API_KEY
 const PASSWORD = process.env.PASSWORD
+const DOMAIN = process.env.DOMAIN
 
 const MONGODB_URI = process.env.NODE_ENV === 'test'
   ? process.env.TEST_MONGODB_URI
@@ -14,5 +15,6 @@ module.exports = {
   MONGODB_URI,
   OPENAI_API_KEY,
   PEXELS_API_KEY,
-  PASSWORD
+  PASSWORD,
+  DOMAIN
 }
