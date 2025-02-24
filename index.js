bf6fef42df3641e7794d15278c7da0cb2ff44b66
sourcyed <31018609+sourcyed@users.noreply.github.com>
2025-02-24 15:50:10 +0200

    Fix line endings

--- index.js
+++ index.js
@@ -5,16 +5,16 @@ const app = require('./app')
 const https = require('https')
 const fs = require('fs')
 
-const DOMAIN = config.DOMAIN 
+const DOMAIN = config.DOMAIN
 let PORT = config.PORT || 3001
 
 let server = app
 
 if (DOMAIN) {
   const options = {
-      key: fs.readFileSync(`/etc/letsencrypt/live/${DOMAIN}/privkey.pem`),
-      cert: fs.readFileSync(`/etc/letsencrypt/live/${DOMAIN}/cert.pem`),
-      ca: fs.readFileSync(`/etc/letsencrypt/live/${DOMAIN}/fullchain.pem`)
+    key: fs.readFileSync(`/etc/letsencrypt/live/${DOMAIN}/privkey.pem`),
+    cert: fs.readFileSync(`/etc/letsencrypt/live/${DOMAIN}/cert.pem`),
+    ca: fs.readFileSync(`/etc/letsencrypt/live/${DOMAIN}/fullchain.pem`)
   }
   server = https.createServer(options,app)
   PORT = 443
